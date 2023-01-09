f8c48b23b12fc89693f84612733d3a0401d327f4
Anthony Chen <50970130+achen318@users.noreply.github.com>
2023-01-09 17:21:34 -0500

    Initialize new website project
    Co-authored-by: thegu5 <thegu5@users.noreply.github.com>
    Co-authored-by: Kelvin Zhao <t1rbr@users.noreply.github.com>

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'StuyPulse';
+export const SITE_DESCRIPTION =
+  'FRC Team 694, or StuyPulse, is an award-winning FIRST robotics team from New York City.';
