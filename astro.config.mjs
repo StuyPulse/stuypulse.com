37154faa2e05f4854a189711f939a7f5075d0805
Shroomicus <linvincent969@gmail.com>
2023-01-12 00:31:17 -0500

    Continue work on making a better carousel.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
