d0957e37715e7bb17fd0c293b051b894c42cb270
thegu5 <58223632+thegu5@users.noreply.github.com>
2023-01-27 12:11:57 -0500

    set to new subdomain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import yaml from '@rollup/plugin-yaml';
 // https://astro.build/config
 export default defineConfig({
   // Change this when the site is deployed to the real domain
-  site: 'https://stuypulse.github.io',
-  base: 'stuypulse.com',
+  site: 'https://new.stuypulse.com',
   integrations: [mdx(), sitemap()],
   vite: {
     plugins: [yaml()]
